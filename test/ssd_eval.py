afd2b93513bf14627ce3e23a891054c0bdeab081
QiYao <oqiyao@gmail.com>
2018-12-19 09:40:31 +0800

    eval code update

--- test/ssd_eval.py
+++ test/ssd_eval.py
@@ -1,5 +1,5 @@
 import os
-#os.environ['CUDA_VISIBLE_DEVICES']='-1'
+os.environ['CUDA_VISIBLE_DEVICES']='-1'
 import cv2
 import numpy as np
 import tensorflow as tf
@@ -27,8 +27,15 @@ box, prob = builder.pipeline.decode_for_ap(net.outputs, config['model']['num_cla
 color = [(255, 0, 0)]
 saver = tf.train.Saver(name="saver")
 num_classes = config['model']['num_classes'] + 1
-num_images = 200
-f = open('fish_det.txt', 'w')
+
+# txt = ['aeroplane', 'bicycle', 'bird', 'boat', 'bottle',
+#     'bus', 'car', 'cat', 'chair', 'cow', 'diningtable',
+#     'dog', 'horse', 'motorbike', 'person', 'pottedplant',
+#     'sheep', 'sofa', 'train', 'tvmonitor']
+txt = ['fish']
+fs = []
+for i in range(len(txt)):
+    fs.append(open('{}_det.txt'.format(txt[i]), 'w'))
 s = 1
 with tf.Session() as sess:
     sess.run(tf.local_variables_initializer())
@@ -41,7 +48,7 @@ with tf.Session() as sess:
             im = (im * 128 + 128).astype(np.uint8)[0, :]
             im = cv2.resize(im, (500, 375))
             name = name[0].decode()
-            for bb, pp, c in zip(b, p, color):
+            for bb, pp, f in zip(b, p, fs):
                 for bbb, ppp in zip(bb, pp):
                     xmin = max(int(bbb[1] * 500) + 1, 0)
                     ymin = max(int(bbb[0] * 375) + 1, 0)
@@ -55,29 +62,36 @@ with tf.Session() as sess:
                     xmax = int(xc + w)
                     ymin = int(yc - h)
                     ymax = int(yc + h)
-                    if ppp > 0.9:
-                        cv2.rectangle(im, (xmin, ymin), (xmax, ymax), c, 2)
-
+                    if ppp > 0.11:
+                        cv2.rectangle(im, (xmin, ymin), (xmax, ymax), color[0], 2)
                     f.write('{:s} {:f} {:d} {:d} {:d} {:d}\n'.format(name, ppp, xmin, ymin, xmax, ymax))
                     a = 1
             cv2.imshow("", im)
-            cv2.waitKey(0)
+            cv2.waitKey(1)
         except tf.errors.OutOfRangeError as e:
-            f.close()
+            for f in fs:
+                f.close()
             break
-rec, prec, ap = voc.voc_eval("/home/yqi/workspace/DL-tools/test/fish_det.txt",
-                             "/home/yqi/Desktop/WineDownloads/大作业3/to student/FishData_subset/Annotations/{:s}.xml",
-                             "/home/yqi/Desktop/WineDownloads/大作业3/to student/FishData_subset/ImageSets/test.txt",
-                             "fish",
-                             ".",
-                             ovthresh=0.5,
-                             use_07_metric=False)
+mAP = 0
+for n in txt:
+    rec, prec, ap = voc.voc_eval("/home/yqi/workspace/DL-tools/test/{}_det.txt".format(n),
+                                 "/home/yqi/Desktop/WineDownloads/大作业3/to student/FishData_subset/Annotations/{:s}.xml",
+                                 "/home/yqi/Desktop/WineDownloads/大作业3/to student/FishData_subset/ImageSets/Main/test.txt",
+                                 n,
+                                 ".",
+                                 ovthresh=0.5,
+                                 use_07_metric=False)
+    plt.figure()
+    plt.plot(rec, prec)
+    plt.xlim(0, 1)
+    plt.xlabel('Recall', fontsize=14)
+    plt.ylabel('Precision', fontsize=14)
+    plt.ylim(0, 1)
+    plt.grid()
+    plt.text(0.1, 1.02, n, fontsize=14)
+    plt.text(0.82, 1.02, 'AP=%.02f%%' % (ap * 100), fontsize=14)
+    plt.savefig(n+'.png')
 
-plt.plot(rec, prec)
-plt.xlim(0, 1)
-plt.xlabel('Recall', fontsize=14)
-plt.ylabel('Precision', fontsize=14)
-plt.ylim(0, 1)
-plt.grid()
-plt.text(0.82, 1.02, 'AP=%.02f%%' % (ap * 100), fontsize=14)
-plt.show()
+    mAP += ap / len(txt)
+    print('AP for {}:'.format(n), ap)
+print("mAP:", mAP)
